d331d9dbbef430834e2e00bba9c9fa2719edd899
bohdants96 <bohdantsisinskyi@gmail.com>
2024-05-21 16:14:17 +0300

    Location and business type endpoints

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.routes import items, login, users, utils, country
+from app.api.routes import items, login, users, utils, country, locations, business_types
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
@@ -8,3 +8,5 @@ api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
 api_router.include_router(items.router, prefix="/items", tags=["items"])
 api_router.include_router(country.router, prefix="/country", tags=["country"])
+api_router.include_router(locations.router, prefix="/locations", tags=["locations"])
+api_router.include_router(business_types.router, prefix="/business_types", tags=["business_types"])
