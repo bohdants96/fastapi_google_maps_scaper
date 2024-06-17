b59922774553466efdb9a5016dbb6bf930ef6bfb
bohdants96 <bohdantsisinskyi@gmail.com>
2024-06-17 13:39:04 +0300

    add read_business_type by name with pagination

--- app/api/routes/business_types/business_types.py
+++ app/api/routes/business_types/business_types.py
@@ -1,11 +1,9 @@
 from fastapi import APIRouter
-from sqlmodel import select
-
-from app.models import PublicBusinessType, BusinessType
-from app.api.deps import CurrentUser, SessionDep
-
 from fastapi_pagination import LimitOffsetPage, paginate
+from sqlmodel import func, select
 
+from app.api.deps import CurrentUser, SessionDep
+from app.models import BusinessType, PublicBusinessType
 
 router = APIRouter()
 
@@ -15,5 +13,16 @@ def read_business_types(
     session: SessionDep, current_user: CurrentUser
 ) -> LimitOffsetPage[PublicBusinessType]:
     statement = select(BusinessType).order_by(BusinessType.name)
+    business_types = session.exec(statement).all()
+    return paginate(business_types)
+
+
+@router.get("/", response_model=LimitOffsetPage[PublicBusinessType])
+def read_business_type(
+    name: str, session: SessionDep, current_user: CurrentUser
+) -> LimitOffsetPage[PublicBusinessType]:
+    statement = select(BusinessType).where(
+        func.lower(BusinessType.name).ilike(f"%{name.lower()}%")
+    )
     business_types = session.exec(statement).all()
     return paginate(business_types)
