9679509a6aedacf1eae8a5054903bf0a62e85078
bohdants96 <bohdantsisinskyi@gmail.com>
2024-06-17 13:46:12 +0300

    add order_by name

--- app/api/routes/business_types/business_types.py
+++ app/api/routes/business_types/business_types.py
@@ -9,20 +9,13 @@ router = APIRouter()
 
 
 @router.get("/", response_model=LimitOffsetPage[PublicBusinessType])
-def read_business_types(
-    session: SessionDep, current_user: CurrentUser
-) -> LimitOffsetPage[PublicBusinessType]:
-    statement = select(BusinessType).order_by(BusinessType.name)
-    business_types = session.exec(statement).all()
-    return paginate(business_types)
-
-
-@router.get("/", response_model=LimitOffsetPage[PublicBusinessType])
 def read_business_type(
     name: str, session: SessionDep, current_user: CurrentUser
 ) -> LimitOffsetPage[PublicBusinessType]:
-    statement = select(BusinessType).where(
-        func.lower(BusinessType.name).ilike(f"%{name.lower()}%")
+    statement = (
+        select(BusinessType)
+        .where(func.lower(BusinessType.name).ilike(f"%{name.lower()}%"))
+        .order_by(BusinessType.name)
     )
     business_types = session.exec(statement).all()
     return paginate(business_types)
